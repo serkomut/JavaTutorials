4d222eb2b18eb891649ede8c6d64ff6fe3676d62
Serol GUZEL <serkomut@gmail.com>
2016-02-01 17:04:40 +0200

    java tutorial, first project

---
+++ MyJavaTutorials/src/com/mytutorial/ChapterOne.java
@@ -0,0 +1,28 @@
+package com.mytutorial;
+
+import java.util.*;
+
+/**
+ * Created by sguzel on 01/02/16.
+ */
+public class ChapterOne {
+    public void Numbers(){
+        int[] numbers = new int[] {1, 2, 3, 4, 5};
+        for (int item : numbers) {
+            System.out.println(item);
+        }
+    }
+
+    public void NumbersList(){
+        List<Integer> numbers = new ArrayList<Integer>() {};
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        for (int item : numbers) {
+            System.out.println("Order : " + item);
+        }
+    }
+}
---
+++ MyJavaTutorials/src/com/mytutorial/Main.java
@@ -0,0 +1,13 @@
+package com.mytutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ChapterOne chapterOne = new ChapterOne();
+        System.out.println("Numbers >>> for :");
+        chapterOne.Numbers();
+
+        System.out.println("NumbersList >>> foreach");
+        chapterOne.NumbersList();
+    }
+}
